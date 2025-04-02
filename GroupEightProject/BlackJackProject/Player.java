6a3a78d77b7ef4595f1c2fb7c9077a7b20ea2f35
olivarju <olivaresjp@hotmail.com>
2025-04-01 20:40:53 -0400

    Player.java

--- GroupEightProject/BlackJackProject/Player.java
+++ GroupEightProject/BlackJackProject/Player.java
@@ -1,14 +1,14 @@
-import java.util.Vector;
-
 public class Player {
 	private String _name;
 	private int _score;
 	private Hand _hand;
-	public Game _unnamed_Game_26;
-	public Vector<Hand> _unnamed_java.util.Vector<Hand>_27 = new Vector<Hand>();
 
 	public String getName() {
 		return this._name;
+	}
+
+	public int getScore() {
+		return this._score;
 	}
 
 	public void stand() {
@@ -29,9 +29,5 @@ public class Player {
 
 	public void split() {
 		throw new UnsupportedOperationException();
-	}
-
-	public int getScore() {
-		return this._score;
 	}
 }
