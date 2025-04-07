ee8ca8637ff71fad01b4bb07d9cc7fcceedabdf1
MacLeaGr <macleagr@sheridancollege.ca>
2025-04-07 17:31:45 -0400

    Update Round.java

--- GroupEightProject/src/main/java/ca/sheridancollege/project/Round.java
+++ GroupEightProject/src/main/java/ca/sheridancollege/project/Round.java
@@ -1,10 +1,12 @@
 package ca.sheridancollege.project;
 
-public class Round {
+public class Round
+{
     private HumanPlayer player;
     private Dealer dealer = new Dealer();
     private Deck deck = new Deck();
-    private int roundNumber;
+    private int numWins = 0;
+    private int doubleScore = 0;
 
     public Round(HumanPlayer player, Dealer dealer, Deck deck) 
     {
@@ -15,31 +17,41 @@ public class Round {
 
     public void startRound() 
     {
+        // Clear the previous round's hands
+        player.getHand().clearHand();
+        dealer.getHand().clearHand();
+        
         System.out.println("Dealing cards!!");
 
         player.getHand().addCard(deck.drawCard());
         player.getHand().addCard(deck.drawCard());
-
+        Card pFC = player.getHand().getCards().get(0); // get player first card
+        Card pSC = player.getHand().getCards().get(1); // get player second card
+                
+        System.out.println(player.getName() + " is dealt two cards, " + pFC.getValue() + " of " + pFC.getSuit() + ", and " + pSC.getValue() + " of " + pSC.getSuit() );
+        
         dealer.getHand().addCard(deck.drawCard());
         dealer.getHand().addCard(deck.drawCard());
-        Card dealerFirstCard = dealer.getHand().getCards().get(0); // get first card dealer drew
+        Card dFC = dealer.getHand().getCards().get(0); // get dealer first card
         
-        System.out.println("Dealer draws one card face up, " + dealerFirstCard.getValue() + " of " + dealerFirstCard.getSuit());
-        System.out.println("Dealer draws one card face down.");
+        Card dSC = dealer.getHand().getCards().get(1); // get dealer second card, added at professor's request
         
-        player.playTurn(deck); // needs implementation in player
+        System.out.println("Dealer draws one card face up, " + dFC.getValue() + " of " + dFC.getSuit());
+        // System.out.println("Dealer draws one card face down."); // regular code
+        System.out.println("Dealer draws one card face down."+ dSC.getValue() + " of " + dSC.getSuit()); // dealer card is revealed on professor's request
+        
+        player.playTurn(deck);
 
         // If player hasn't busted, dealer plays
         if (!player.getHand().isBust())
         {
-            Card dealerSecondCard = dealer.getHand().getCards().get(1); // get second card dealer drew
-            System.out.println("Dealer reveals second card, " + dealerSecondCard.getValue() + " of " + dealerSecondCard.getSuit());
+            // Card dSC = dealer.getHand().getCards().get(1); replaced with overwrite at professor's request
+            dSC = dealer.getHand().getCards().get(1); // get dealer second card
+            System.out.println("Dealer reveals second card, " + dSC.getValue() + " of " + dSC.getSuit());
             System.out.println("Dealer Hand Value = " + dealer.hand.getHandValue());
             dealer.playTurn(deck);
         }
-
         printRoundResult();
-        roundNumber++;
     }
 
     // Get the final scores of player and dealer
@@ -47,18 +59,28 @@ public class Round {
     {	
         int playerHandValue = player.getHand().getHandValue();
         int dealerHandValue = dealer.getHand().getHandValue();
+        if(player.getDouble()) // if player doubled down returns true
+        {
+            doubleScore = 2;
+        }
+        else
+        {
+            doubleScore = 1;
+        }
 
         if (player.getHand().isBust())
         {
-            System.out.println("Player busts! Dealer wins.");
+            System.out.println("Player busts! Dealer wins with " + dealerHandValue);
 	}
         else if (dealer.getHand().isBust())
         {
-            System.out.println("Dealer busts! Player wins.");
+            System.out.println("Dealer busts! Player wins with " + playerHandValue);
+            numWins += doubleScore; // if player wins, doubleScore is added
 	}
         else if (playerHandValue > dealerHandValue)
         {
             System.out.println("Player wins with " + playerHandValue);
+            numWins += doubleScore; // if player wins, doubleScore is added
 	}
         else if (dealerHandValue > playerHandValue)
         {
@@ -68,5 +90,10 @@ public class Round {
         {
             System.out.println("It's a tie!");
         }
+    }
+    
+    public int getNumWins()
+    {
+        return this.numWins;
     }
 }
